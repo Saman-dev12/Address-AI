6f3a6b008ed62d03c0a84ca096cf06bd7e3c466e
Sourav-Goyal19 <goyalsourav935@gmail.com>
2024-12-28 01:33:33 +0530

    minor update

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -42,22 +42,22 @@ const DashboardPage = async () => {
   );
 };
 
-export const DataCardLoading = () => {
-  return (
-    <Card className="borer-none drop-shadow-sm h-[192px]">
-      <CardHeader className="flex flex-row items-center justify-between gap-x-4">
-        <div className="space-y-2">
-          <Skeleton className="h-6 w-24" />
-          <Skeleton className="h-4 w-40" />
-        </div>
-        <Skeleton className="size-12" />
-      </CardHeader>
-      <CardContent>
-        <Skeleton className="shrink-0 h-10 w-24 mb-2" />
-        <Skeleton className="shrink-0 h-4 w-40" />
-      </CardContent>
-    </Card>
-  );
-};
+// export const DataCardLoading = () => {
+//   return (
+//     <Card className="borer-none drop-shadow-sm h-[192px]">
+//       <CardHeader className="flex flex-row items-center justify-between gap-x-4">
+//         <div className="space-y-2">
+//           <Skeleton className="h-6 w-24" />
+//           <Skeleton className="h-4 w-40" />
+//         </div>
+//         <Skeleton className="size-12" />
+//       </CardHeader>
+//       <CardContent>
+//         <Skeleton className="shrink-0 h-10 w-24 mb-2" />
+//         <Skeleton className="shrink-0 h-4 w-40" />
+//       </CardContent>
+//     </Card>
+//   );
+// };
 
 export default DashboardPage;
